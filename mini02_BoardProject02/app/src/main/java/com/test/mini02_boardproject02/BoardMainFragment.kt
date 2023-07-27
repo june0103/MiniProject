3b517ead7ae308a0ac580a13cc2a7967f468795c
June <rhwns0103@naver.com>
2023-07-27 19:42:37 +0900

    feat: 게시판 화면전환, 서치바, 서치뷰 및 리사이클러뷰 배치

--- mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/BoardMainFragment.kt
+++ mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/BoardMainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.transition.MaterialSharedAxis
 import com.test.mini02_boardproject02.databinding.FragmentBoardMainBinding
 import com.test.mini02_boardproject02.databinding.HeaderBoardMainBinding
 
@@ -12,6 +14,14 @@ class BoardMainFragment : Fragment() {
 
     lateinit var fragmentBoardMainBinding: FragmentBoardMainBinding
     lateinit var mainActivity: MainActivity
+
+    var newFragment: Fragment? = null
+    var oldFragment: Fragment? = null
+
+    companion object {
+        val POST_LIST_FRAGMENT = "PostListFragment"
+        val MODIFY_USER_FRAGMENT = "ModifyUserFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +33,9 @@ class BoardMainFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         fragmentBoardMainBinding.run {
+
+            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+
             toolbarBoardMain.run {
                 title = "게시판 메인"
                 setNavigationIcon(R.drawable.menu_24px)
@@ -30,11 +43,11 @@ class BoardMainFragment : Fragment() {
                     // 네비게이션 뷰
                     drawerLayoutBoardMain.open()
                 }
-
             }
 
             // DrawerView
             navigationViewBoardMain.run {
+
                 // 헤더설정
                 val headerBoardMainBinding = HeaderBoardMainBinding.inflate(inflater)
                 headerBoardMainBinding.textViewHeaderBoardMainNickName.text = "아무개님"
@@ -47,50 +60,121 @@ class BoardMainFragment : Fragment() {
                     // it.isChecked = true
 
                     // 선택한 메뉴 id로 분기
-                    when(it.itemId) {
+                    when (it.itemId) {
                         // 전체게시판
                         R.id.item_board_main_all -> {
-                            textViewTest.text = "전체게시판"
+                            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 자유게시판
                         R.id.item_board_main_free -> {
-                            textViewTest.text = "자유게시판"
+                            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 유머게시판
                         R.id.item_board_main_gag -> {
-                            textViewTest.text = "유머게시판"
+                            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 질문게시판
                         R.id.item_board_main_qna -> {
-                            textViewTest.text = "질문게시판"
+                            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 스포츠게시판
                         R.id.item_board_main_sports -> {
-                            textViewTest.text = "스포츠게시판"
+                            replaceFragment(POST_LIST_FRAGMENT, false, false, null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 사용자 정보 수정
                         R.id.item_board_main_user_info -> {
-                            mainActivity.replaceFragment(MainActivity.MODIFY_USER_FRAGMENT, true, null)
+                            replaceFragment(MODIFY_USER_FRAGMENT,true, false,null)
+                            drawerLayoutBoardMain.close()
                         }
                         // 로그아웃
                         R.id.item_board_main_logout -> {
-                            mainActivity.removeFragment(MainActivity.BOARD_MAIN_FRAGMENT)
+                            mainActivity.replaceFragment(MainActivity.LOGIN_FRAGMENT, false, null)
                         }
                         // 회원탈퇴
                         R.id.item_board_main_sign_out -> {
-                            mainActivity.removeFragment(MainActivity.BOARD_MAIN_FRAGMENT)
+                            mainActivity.replaceFragment(MainActivity.LOGIN_FRAGMENT, false, null)
                         }
                     }
 
                     // 닫아준다
-                    drawerLayoutBoardMain.close()
+//                    drawerLayoutBoardMain.close()
 
-                    true
+                    false
                 }
             }
         }
-
         return fragmentBoardMainBinding.root
     }
 
+    // 지정한 Fragment를 보여주는 메서드
+    fun replaceFragment(name: String, addToBackStack: Boolean, animate: Boolean, bundle: Bundle?) {
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = mainActivity.supportFragmentManager.beginTransaction()
+
+        // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
+        if (newFragment != null) {
+            oldFragment = newFragment
+        }
+
+        // 새로운 Fragment를 담을 변수
+        newFragment = when (name) {
+            POST_LIST_FRAGMENT -> PostListFragment()
+            MODIFY_USER_FRAGMENT -> ModifyUserFragment()
+
+            else -> Fragment()
+        }
+
+        newFragment?.arguments = bundle
+
+        if (animate == true) {
+            // 애니메이션 설정
+            if (oldFragment != null) {
+                oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+                oldFragment?.enterTransition = null
+                oldFragment?.returnTransition = null
+            }
+
+            newFragment?.exitTransition = null
+            newFragment?.reenterTransition = null
+            newFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+            newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        } else {
+            if (oldFragment != null) {
+                oldFragment?.exitTransition = null
+                oldFragment?.reenterTransition = null
+                oldFragment?.enterTransition = null
+                oldFragment?.returnTransition = null
+            }
+
+            newFragment?.exitTransition = null
+            newFragment?.reenterTransition = null
+            newFragment?.enterTransition = null
+            newFragment?.returnTransition = null
+        }
+        // Fragment를 교채한다.
+        fragmentTransaction.replace(R.id.boardMainContainer, newFragment!!)
+
+        if (addToBackStack == true) {
+            // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+            fragmentTransaction.addToBackStack(name)
+        }
+
+        // 교체 명령이 동작하도록 한다.
+        fragmentTransaction.commit()
+    }
+
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name: String) {
+        mainActivity.supportFragmentManager.popBackStack(
+            name,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
+    }
 }
--- mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/PostListFragment.kt
+++ mini02_BoardProject02/app/src/main/java/com/test/mini02_boardproject02/PostListFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.test.mini02_boardproject02.databinding.FragmentPostListBinding
+import com.test.mini02_boardproject02.databinding.RowPostListBinding
 
 class PostListFragment : Fragment() {
 
@@ -16,10 +20,92 @@ class PostListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         fragmentPostListBinding = FragmentPostListBinding.inflate(inflater)
 
+        fragmentPostListBinding.run {
+            recyclerViewPostListAll.run {
+                adapter = AllRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+
+            recyclerViewPostListResult.run {
+                adapter = ResultRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+
+
         return fragmentPostListBinding.root
+    }
+
+    // 모든 게시글 목록을 보여주는 리사이클러뷰 어댑터
+    inner class AllRecyclerViewAdapter:RecyclerView.Adapter<AllRecyclerViewAdapter.AllViewHolder>(){
+
+        inner class AllViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+            val rowPostListSubject:TextView
+            val rowPostListNickName:TextView
+
+            init{
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = AllViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: AllViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
+    }
+
+
+    // 검색 결과 게시글 목록을 보여주는 리사이클러 뷰의 어뎁터
+    inner class ResultRecyclerViewAdapter : RecyclerView.Adapter<ResultRecyclerViewAdapter.ResultViewHolder>(){
+        inner class ResultViewHolder(rowPostListBinding: RowPostListBinding) : RecyclerView.ViewHolder(rowPostListBinding.root){
+
+            val rowPostListSubject:TextView
+            val rowPostListNickName:TextView
+
+            init{
+                rowPostListSubject = rowPostListBinding.rowPostListSubject
+                rowPostListNickName = rowPostListBinding.rowPostListNickName
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            val allViewHolder = ResultViewHolder(rowPostListBinding)
+
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: ResultViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다 : $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
     }
 
 
